f7776b0e0afa993984fbb3864cb4b1fcbb360d32
John Mark Untalan <apeiron.jmunts@gmail.com>
2015-08-03 11:17:00 +0800

    Added access_control at application controller and conditional for Nav menu display at layouts/_sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :access_control
+
+  private
+
+  def access_control
+  	# 0 - Military & 1 - Ship
+  	return true if current_user.nil?
+  	if current_user.department == 1
+  		if params["controller"] == "api/rfqs"
+  			redirect_to root_url, alert: "Access denied"
+  		end
+  	elsif current_user.department == 0
+  		if params["controller"] == "api/logreqs"
+  			redirect_to root_url, alert: "Acces denied"
+  		end
+  	else
+  	end
+  end
 end
