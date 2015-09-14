56eff8e7b46d4357237ad92bd31ae6cc0b205a56
reynan albaredo <chezca1228@yahoo.com>
2015-09-14 08:48:35 +0800

    added new gem for paranoia before_destroy

--- app/models/principal.rb
+++ app/models/principal.rb
@@ -4,7 +4,11 @@ class Principal < ActiveRecord::Base
   has_many :rfqs
   has_many :logreqs
 
+  # before_create :check_duplicate_entry
+
   #validations
   validates :name, :contact_number, :email, :address, :point_of_contact, presence: true
-  validates :name, uniqueness: true
+  validates :name, :uniqueness_without_deleted => true
+
+  
 end
