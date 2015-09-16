331f736760e1ff6019ea85dc71678cb9b8942ffc
reynan albaredo <chezca1228@yahoo.com>
2015-09-16 13:21:56 +0800

    staging setup for capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,12 +2,12 @@ set :rails_env, :staging
 set :branch, :staging
 set :deploy_to, '/home/gmls'
 set :user, :gmls
-set :nginx_server_name, 'gmls@cloudapp.net'
+set :nginx_server_name, 'gmls.cloudapp.net'
 # set :default_env, { 'SECRET_KEY_BASE' => '6277cfb7ba35b74cc7b4e617c65d0288898f8cf7152b2c5a72c19397801d8030db8952aeaf6320bd31c10da478583d937160d378652fb9143f0edd1d66a549c5' } 
 set :linked_files, %w[config/database.yml tmp/pids/unicorn.pid log/unicorn.log]
 # set :linked_dirs, %w{ public/paperclip }
 
-server 'gmls@cloudapp.net', user: 'gmls', roles: %w[web app]
+server 'gmls.cloudapp.net', user: 'gmls', roles: %w[web app]
 
 
 set :ssh_options, {
