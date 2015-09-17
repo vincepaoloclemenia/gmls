331c101ef7e32db398f91aa005ca630898850540
reynan albaredo <chezca1228@yahoo.com>
2015-09-17 14:31:24 +0800

    configuration on staging environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,5 +68,39 @@ namespace :deploy do
     end
   end
 
+  # set :application, 'gmls'
+  # set :repo_url, 'git@bitbucket.org:reynan_albaredo/gmls-v2.git'
+
+  # set :linked_files, %w{config/database.yml .env}
+  # set :linked_dirs, %w{tmp/pids}
+
+  # set :unicorn_config_path, "config/unicorn.rb"
+
+  # set :rbenv_type, :user # or :system, depends on your rbenv setup
+  # set :rbenv_ruby, '2.2.1'
+  # set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+  # set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+  # set :rbenv_roles, :all # default value
+
+  # namespace :deploy do
+
+  #   desc 'Restart application'
+  #   task :restart do
+  #     on roles(:app), in: :sequence, wait: 5 do
+  #       invoke 'unicorn:restart'
+  #     end
+  #   end
+
+  #   after :publishing, :restart
+
+  #   after :restart, :clear_cache do
+  #     on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #       # Here we can do anything such as:
+  #       # within release_path do
+  #       #   execute :rake, 'cache:clear'
+  #       # end
+  #     end
+  #   end
+
 end
 
